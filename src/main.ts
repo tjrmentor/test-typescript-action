e7412cb1e3afe88ec0344b2a39ed7cd8b9782bbf
Taylor Richard <taylor.richard@hyland.com>
2022-11-03 18:00:57 -0400

    another one

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 async function run(): Promise<void> {
-  core.notice(JSON.stringify(github.context))
+  console.log(github.context)
 }
 
 run()
